dbe6a35ffb737e475a4203ed12b57812961709ae
3lM4RC <marc78859@gmail.com>
2023-10-07 23:28:13 -0600

    Ahora el programa tiene coordenadas impresas, y las funciones se encuentran en el archivo Functions.py

---
+++ Practica 1/functions.py
@@ -0,0 +1,114 @@
+import pygame
+import sys
+import tkinter as tk
+from tkinter import filedialog
+import pandas as pd
+import ast
+import string
+
+
+# Colores
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+GRIS = (128,128,128)
+VERDE = (150,210,80)
+AZUL = (0, 175, 255)  # Color azul
+CARNE = (250, 191, 143)
+ROJO = (255, 0, 0)  # Color rojo
+NARANJA = (255, 192, 0)  # Color amarillo
+
+
+def calcular_alto(laberinto):
+    ALTO = (len(laberinto) * 40)
+    return ALTO
+
+def calcular_ancho(laberinto):
+    ANCHO = (len(laberinto) * 40)
+    return ANCHO
+
+
+def pedir_laberinto():
+    # Crear una ventana principal
+    root = tk.Tk()
+    root.withdraw()  # Ocultar la ventana principal
+
+    # Abrir el cuadro de diálogo para seleccionar un archivo TXT
+    file_path = filedialog.askopenfilename(filetypes=[("Archivos TXT", "*.txt")])
+
+    if file_path:
+        # Leer el contenido del archivo TXT y eliminar comas y paréntesis
+        with open(file_path, 'r') as file:
+            text = file.read()
+
+        # Convertir el texto en una lista anidada de Python utilizando ast.literal_eval
+        try:
+            matrix = ast.literal_eval(text)
+            # Ahora, 'matrix' contiene la matriz bidimensional de Python
+            print("Matriz cargada correctamente en la variable 'matrix'.")
+            return matrix
+        except Exception as e:
+            print(f"Error al cargar la matriz: {str(e)}")
+    else:
+        print("No se seleccionó ningún archivo TXT.")
+
+def crear_laberinto():
+    laberinto = pedir_laberinto()
+
+    #Aqui es donde ponemos las coordenadas laterales con numeros
+    cantidad_numeros = len(laberinto[0])
+    for i in range(cantidad_numeros):
+        laberinto[i] = [i+1] + laberinto[i]
+
+    #Aqui es donde ponemos las coordenadas superiores con letras
+    cantidad_letras = len(laberinto)
+    letras = list(string.ascii_lowercase)[:cantidad_letras]
+    laberinto.insert(0,[" "] + letras)
+
+    # Imprimir el laberinto con la nueva columna
+    for fila in laberinto:
+        print(fila)
+    return laberinto
+
+
+def dibujar_laberinto(laberinto,screen, start, end, x, y):
+    Sx, Sy = start
+    Ex, Ey = end
+    for fila in range(len(laberinto)):
+        for columna in range(len(laberinto[0])):
+            if (fila == 0) | (columna == 0):
+
+                #pygame.draw.rect(screen, GRIS, (columna * 40, fila * 40, 40, 40))  
+                #if isinstance(laberinto[fila][columna], str):
+                # Si el contenido en laberinto[fila][columna] es una letra, imprímela
+
+                coord = str(laberinto[fila][columna])
+                pygame.draw.rect(screen, GRIS, (columna * 40, fila * 40, 40, 40))
+                font = pygame.font.Font("C:/Windows/Fonts/arial.ttf", 24)  # Fuente y tamaño de la letra
+                texto = font.render(coord, True, NEGRO)  # Color del texto (negro)
+                text_rect = texto.get_rect()
+                text_rect.center = (columna * 40 + 20, fila * 40 + 20)  # Centro del rectángulo
+                screen.blit(texto, text_rect)  # Dibuja el texto en el centro del rectángulo
+            else: 
+                if laberinto[fila][columna] == 0:
+                    color = GRIS
+                elif laberinto[fila][columna] == 2:
+                    color = VERDE
+                elif laberinto[fila][columna] == 3:
+                    color = CARNE
+                elif laberinto[fila][columna] == 4:
+                    color = AZUL
+                elif laberinto[fila][columna] == 5:
+                    color = NARANJA
+                else: 
+                    color = BLANCO
+                pygame.draw.rect(screen, color, (columna * 40, fila * 40, 40, 40))
+
+    # Dibujar la entrada (azul) en [14][3]
+    pygame.draw.rect(screen, AZUL, (Sx,Sy, 40, 40))
+
+    # Dibujar la salida (rojo) en [14][1]
+    pygame.draw.rect(screen, ROJO, (Ex,Ey, 40, 40))
+
+    # Dibujar el punto amarillo en la posición actual
+    pygame.draw.rect(screen, NARANJA, (x, y, 40, 40))
+    pygame.display.flip()
--- Practica 1/main1.py
+++ Practica 1/main1.py
@@ -1,132 +1,53 @@
-import pygame
-import sys
-import tkinter as tk
-from tkinter import filedialog
-import pandas as pd
-import ast
+from functions import *
 
-def pedir_laberinto():
-
-    # Crear una ventana principal
-    root = tk.Tk()
-    root.withdraw()  # Ocultar la ventana principal
-
-    # Abrir el cuadro de diálogo para seleccionar un archivo TXT
-    file_path = filedialog.askopenfilename(filetypes=[("Archivos TXT", "*.txt")])
-
-    if file_path:
-        # Leer el contenido del archivo TXT y eliminar comas y paréntesis
-        with open(file_path, 'r') as file:
-            text = file.read()
-
-        # Convertir el texto en una lista anidada de Python utilizando ast.literal_eval
-        try:
-            matrix = ast.literal_eval(text)
-            # Ahora, 'matrix' contiene la matriz bidimensional de Python
-            print("Matriz cargada correctamente en la variable 'matrix'.")
-            return matrix
-        except Exception as e:
-            print(f"Error al cargar la matriz: {str(e)}")
-    else:
-        print("No se seleccionó ningún archivo TXT.")
-
+laberinto = crear_laberinto()
 
 # Dimensiones del laberinto (ancho y alto)
-ANCHO = 600
-ALTO = 600
-
-# Colores
-BLANCO = (255, 255, 255)
-NEGRO = (0, 0, 0)
-GRIS = (128,128,128)
-VERDE = (150,210,80)
-AZUL = (0, 175, 255)  # Color azul
-CARNE = (250, 191, 143)
-ROJO = (255, 0, 0)  # Color rojo
-NARANJA = (255, 192, 0)  # Color amarillo
-
-
-laberinto = pedir_laberinto()
-# Laberinto
-'''laberinto = [
-    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
-    [0,1,1,1,1,0,1,1,1,1,1,1,1,1,1],
-    [0,1,0,1,0,0,1,0,1,0,0,0,0,0,0],
-    [0,0,1,1,1,1,1,0,1,0,0,0,0,0,0],
-    [0,0,0,1,0,1,0,0,1,0,0,0,0,0,0],
-    [0,0,0,1,0,0,0,0,1,0,0,0,0,0,0],
-    [0,1,1,1,1,1,1,0,1,0,0,0,0,0,0],
-    [0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
-    [0,1,0,1,1,1,1,1,1,0,0,0,0,0,0],
-    [1,1,0,1,0,0,0,1,0,0,0,0,0,0,0],
-    [0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
-    [0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
-    [0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
-    [0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
-    [0,0,0,1,0,0,0,1,0,0,0,0,0,0,0]
-]'''
-def dibujar_laberinto():
-    for fila in range(len(laberinto)):
-        for columna in range(len(laberinto[0])):
-            if laberinto[fila][columna] == 0:
-                pygame.draw.rect(screen, GRIS, (columna * 40, fila * 40, 40, 40))
-            elif laberinto[fila][columna] == 2:
-                pygame.draw.rect(screen, VERDE, (columna * 40, fila * 40, 40, 40))
-            elif laberinto[fila][columna] == 3:
-                pygame.draw.rect(screen, CARNE, (columna * 40, fila * 40, 40, 40))
-            elif laberinto[fila][columna] == 4:
-                pygame.draw.rect(screen, AZUL, (columna * 40, fila * 40, 40, 40))
-            elif laberinto[fila][columna] == 5:
-                pygame.draw.rect(screen, NARANJA, (columna * 40, fila * 40, 40, 40))
-
-            else:
-                pygame.draw.rect(screen, BLANCO, (columna * 40, fila * 40, 40, 40))
-
-    # Dibujar la entrada (azul) en [14][3]
-    pygame.draw.rect(screen, AZUL, (3 * 40, 14 * 40, 40, 40))
-
-    # Dibujar la salida (rojo) en [14][1]
-    pygame.draw.rect(screen, ROJO, (14 * 40, 1 * 40, 40, 40))
-
-    # Dibujar el punto amarillo en la posición actual
-    pygame.draw.rect(screen, NARANJA, (posicion_x, posicion_y, 40, 40))
-    pygame.display.flip()
+ANCHO = calcular_ancho(laberinto)
+ALTO = calcular_alto(laberinto)
 
 pygame.init()
 screen = pygame.display.set_mode((ANCHO, ALTO))
-pygame.display.set_caption("Laberinto")
+pygame.display.set_caption("Maze")
 
 reloj = pygame.time.Clock()
 
 # Coordenadas iniciales del punto amarillo (entrada)
-posicion_x = 3 * 40
-posicion_y = 14 * 40
+x = 4 * 40
+y = 15 * 40
+ex = 15 * 40
+ey = 2 * 40
+
+Start = (x, y)
+End = (ex, ey)
 game_over = False
 
+'''Aqui e donde el juego comienza'''
 while not game_over:
+    screen.fill(BLANCO)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        if event.type == pygame.QUIT: ##Si presionamos la X o quitamos el juego:
+            pygame.quit() #Abortamos el pygame
+            sys.exit() #Sliamos del programa
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                if posicion_x - 40 >= 0 and laberinto[posicion_y // 40][posicion_x // 40 - 1] == 1:
-                    posicion_x -= 40
+                if x - 40 >= 0 and laberinto[y // 40][x // 40 - 1] == 1:
+                    x -= 40
             elif event.key == pygame.K_RIGHT:
-                if posicion_x + 40 < ANCHO and laberinto[posicion_y // 40][posicion_x // 40 + 1] == 1:
-                    posicion_x += 40
+                if x + 40 < ANCHO and laberinto[y // 40][x // 40 + 1] == 1:
+                    x += 40
             elif event.key == pygame.K_UP:
-                if posicion_y - 40 >= 0 and laberinto[posicion_y // 40 - 1][posicion_x // 40] == 1:
-                    posicion_y -= 40
+                if y - 40 >= 0 and laberinto[y // 40 - 1][x // 40] == 1:
+                    y -= 40
             elif event.key == pygame.K_DOWN:
-                if posicion_y + 40 < ALTO and laberinto[posicion_y // 40 + 1][posicion_x // 40] == 1:
-                    posicion_y += 40
-
-    dibujar_laberinto()
+                if y + 40 < ALTO and laberinto[y // 40 + 1][x // 40] == 1:
+                    y += 40
+    
+    dibujar_laberinto(laberinto, screen, Start, End, x, y)
     reloj.tick(60)
 
     # Verificar si el cuadro amarillo llega al cuadro rojo
-    if (posicion_x // 40, posicion_y // 40) == (14, 1):
+    if (x, y) == (ex, ey):
         game_over = True
 
 print("¡Laberinto resuelto!")
